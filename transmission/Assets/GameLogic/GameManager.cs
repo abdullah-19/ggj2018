cade56404336a9666235d72d7b514d765abb9dc0
Julius Cassin <juliuscassin@Juliuss-MacBook-Pro.local>
2018-01-27 16:20:00 -0500

    Weather manager finished

--- transmission/Assets/GameLogic/GameManager.cs
+++ transmission/Assets/GameLogic/GameManager.cs
@@ -6,41 +6,41 @@ using UnityEngine.Networking;
 public class GameManager : NetworkBehaviour {
   private TimeController timeController;
 
-  void Start () {
-    SetUpComponents();
-    // When other player connects...
-    StartCoroutine(GetReady());
-    Debug.Log("Created Gallery of " + allRounds.Count() + " rounds. From codeblock of length " + codeBlock.Length);
-  }
-
-  IEnumerator GetReady() {
-    SetUpRound(allRounds);
-    PlayMusic();
-    streakNotifier.DisplayTextOnTopOfScreen((allRounds.Count() > 1 ? (allRounds.Count() + 1) + " Rounds" : "1 Round"), 3);
-    yield return new WaitForSecondsRealtime(2);
-    streakNotifier.DisplayTextOnTopOfScreen(codeBlock.Length + " Chars", 3);
-    yield return new WaitForSecondsRealtime(2);
-    announcer.PlayGetReadySound();
-    streakNotifier.DisplayTextOnTopOfScreen("Get Ready", 2);
-    yield return new WaitForSecondsRealtime(1);
-    streakNotifier.DisplayTextOnTopOfScreen("3", 1);
-    yield return new WaitForSecondsRealtime(1);
-    streakNotifier.DisplayTextOnTopOfScreen("2", 1);
-    yield return new WaitForSecondsRealtime(1);
-    streakNotifier.DisplayTextOnTopOfScreen("1", 1);
-    PlayReloadSound();
-    yield return new WaitForSecondsRealtime(1);
-    announcer.PlayBeginSound();
-    streakNotifier.DisplayTextOnTopOfScreen("BEGIN", 1);
-    StartRound();
-  }
+//  void Start () {
+//    SetUpComponents();
+//    // When other player connects...
+//    StartCoroutine(GetReady());
+//    Debug.Log("Created Gallery of " + allRounds.Count() + " rounds. From codeblock of length " + codeBlock.Length);
+//  }
+//
+//  IEnumerator GetReady() {
+//    SetUpRound(allRounds);
+//    PlayMusic();
+//    streakNotifier.DisplayTextOnTopOfScreen((allRounds.Count() > 1 ? (allRounds.Count() + 1) + " Rounds" : "1 Round"), 3);
+//    yield return new WaitForSecondsRealtime(2);
+//    streakNotifier.DisplayTextOnTopOfScreen(codeBlock.Length + " Chars", 3);
+//    yield return new WaitForSecondsRealtime(2);
+//    announcer.PlayGetReadySound();
+//    streakNotifier.DisplayTextOnTopOfScreen("Get Ready", 2);
+//    yield return new WaitForSecondsRealtime(1);
+//    streakNotifier.DisplayTextOnTopOfScreen("3", 1);
+//    yield return new WaitForSecondsRealtime(1);
+//    streakNotifier.DisplayTextOnTopOfScreen("2", 1);
+//    yield return new WaitForSecondsRealtime(1);
+//    streakNotifier.DisplayTextOnTopOfScreen("1", 1);
+//    PlayReloadSound();
+//    yield return new WaitForSecondsRealtime(1);
+//    announcer.PlayBeginSound();
+//    streakNotifier.DisplayTextOnTopOfScreen("BEGIN", 1);
+//    StartRound();
+//  }
 
   void Awake() {
     timeController = transform.GetComponent<TimeController>();
   }
 
-	void Start () {
-    timeController.StartTime(30);
-	}
+//	void Start () {
+//    timeController.StartTime(30);
+//	}
 
 }
