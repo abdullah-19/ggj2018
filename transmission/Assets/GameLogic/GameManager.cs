57de694bc4caa1a2d5cfeac336b8fb310fe0d0de
zc <zachcaceres@gmail.com>
2018-01-27 11:48:43 -0500

    add syncvar clock and game manager

---
+++ transmission/Assets/GameLogic/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour {
+  private TimeController timeController;
+
+  void Awake() {
+    timeController = transform.GetComponent<TimeController>();
+  }
+
+	void Start () {
+    timeController.StartTime(30);
+	}
+
+}
